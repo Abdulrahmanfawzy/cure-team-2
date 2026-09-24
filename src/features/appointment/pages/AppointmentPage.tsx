c87d7ae074a76a41b712f8c61482c7a7c09b9c6b
AyaHassan55 <haya98166@gmail.com>
2026-09-24 05:45:40 +0300

    feat(appointment): implement testimonial ui component

--- src/features/appointment/pages/AppointmentPage.tsx
+++ src/features/appointment/pages/AppointmentPage.tsx
@@ -1,22 +1,30 @@
 import AppointmentHeader from "../component/Appointment/AppointmentHeader"
 import AppointmentPicker from "../component/Appointment/AppointmentPicker"
 import AppointmentRating from "../component/Appointment/AppointmentRating"
+import Testimonial from "../component/Appointment/Testimonial"
 
-const AppointmentPage=()=> {
-  return (
-     <main className="font-[Georgia] mt-27 ml-25">
-      {/* ---------- */}
-      <div className="max-w-196">
-        <AppointmentHeader/>
-        <AppointmentPicker />
-        <AppointmentRating />
-       
+const AppointmentPage = () => {
+    return (
+        <main className="font-[Georgia] mt-27 ml-25 ">
+            {/* ---------- */}
+            <div className="max-w-196">
+                <AppointmentHeader />
+                <AppointmentPicker />
+                <AppointmentRating />
+                {/* <Testimonial /> */}
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 mb-13">
+                    <Testimonial />
+                    <Testimonial />
+                </div>
 
-      
-      </div>
+                
 
-    </main>
-  )
+
+
+            </div>
+
+        </main>
+    )
 }
 
 export default AppointmentPage
