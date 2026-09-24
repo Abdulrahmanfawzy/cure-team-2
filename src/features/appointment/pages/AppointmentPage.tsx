0c04322c9ff11d8efe0e78e405ad7b5f5c635588
AyaHassan55 <haya98166@gmail.com>
2026-09-24 06:13:20 +0300

    feat (appointment) : implement header for doc info part

--- src/features/appointment/pages/AppointmentPage.tsx
+++ src/features/appointment/pages/AppointmentPage.tsx
@@ -1,11 +1,13 @@
+
 import AppointmentHeader from "../component/Appointment/AppointmentHeader"
 import AppointmentPicker from "../component/Appointment/AppointmentPicker"
 import AppointmentRating from "../component/Appointment/AppointmentRating"
 import Testimonial from "../component/Appointment/Testimonial"
+import DoctorHeader from "../component/DoctorProfile/DoctorHeader"
 
 const AppointmentPage = () => {
     return (
-        <main className="font-[Georgia] mt-27 ml-25 ">
+        <main className="font-[Georgia] mt-27 mx-15 flex gap-6">
             {/* ---------- */}
             <div className="max-w-196">
                 <AppointmentHeader />
@@ -17,11 +19,17 @@ const AppointmentPage = () => {
                     <Testimonial />
                 </div>
 
-                
+
 
 
 
             </div>
+            {/*----------- doc info----------- */}
+            <div className="w-full max-w-115 rounded-4xl pt-8 px-4 pb-6 bg-[#F5F6F7] ">
+                <DoctorHeader />
+            </div>
+
+
 
         </main>
     )
